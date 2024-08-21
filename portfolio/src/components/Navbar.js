479af98af43978d4d653f3fbd852c08e617be130
MohamedNawfal-7 <mohamednawfal183@gmail.com>
2024-08-21 17:44:18 +0530

    first commit

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import "./Navbar.css"
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="header">
+        <Link to="/">
+            <h1>Portfolio</h1>
+        </Link>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
